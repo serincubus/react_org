a22c3579fc72e9649cde946f85ed0409ea30d17c
serincubus <serincubus2@gmail.com>
2024-12-30 13:56:17 -0300

    trabajando cambio en los inputs, funcionalidad manejarCambio (campoTexto/index.js)

--- src/Componentes/CampoTexto/index.js
+++ src/Componentes/CampoTexto/index.js
@@ -1,11 +1,26 @@
+import {useState} from "react"
 import "./campoTexto.css"
 
 function CampoTexto(props) {
+    const [valor,actualizarValor]=useState("")
     console.log('datos:', props)
     const placeholderModificado = `${props.placeholder}...`
+ 
+     const manejarCambio= (e)=>{
+        console.log("cambio", e.target.value);
+        actualizarValor(e.target.value)
+        
+     }   
+
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={placeholderModificado} required={props.required}/>
+        <input 
+            placeholder={placeholderModificado} 
+            required={props.required} 
+            value={valor}
+            onChange={manejarCambio}
+            />
+
     </div>
 }
 
